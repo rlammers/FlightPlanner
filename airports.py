b98e09125b87508dc8b53c4b64b7a25b627155bc
Roger Lammers <rmlammers@gmail.com>
2018-04-15 17:35:03 +1200

    Distance function added

--- airports.py
+++ airports.py
@@ -1,4 +1,4 @@
-import random
+from geopy import distance
 
 airports = [
             'NZAA',
@@ -15,5 +15,22 @@ airports = [
             'NZTG'
             ]
 
-random.shuffle(airports, random.random)
+# random.shuffle(airports, random.random)
 print(airports)
+
+
+def distance_between(dept_airport, arr_airport):
+    dept_coords = (dept_airport.latitude, dept_airport.longitude)
+    arr_coords = (arr_airport.latitude, dept_airport.longitude)
+    return distance.great_circle(dept_coords, arr_coords).km
+
+
+class Airport(object):
+    icao = ''
+    latitude = 0
+    longitude = 0
+
+    def __init__(self, icao, latitude, longitude):
+        self.icao = icao
+        self.latitude = latitude
+        self.longitude = longitude
