74fa9d9baa2c235e6fb58a61dd05670fe5d8bb76
Roger Lammers <rmlammers@gmail.com>
2018-04-15 17:12:44 +1200

    Shuffled list of 737-800 capable NZ airports

---
+++ airports.py
@@ -0,0 +1,19 @@
+import random
+
+airports = [
+            'NZAA',
+            'NZWN',
+            'NZQN',
+            'NZDN',
+            'NZNV',
+            'NZHN',
+            'NZRO',
+            'NZOH',
+            'NZWP',
+            'NZPM',
+            'NZNR',
+            'NZTG'
+            ]
+
+random.shuffle(airports, random.random)
+print(airports)
