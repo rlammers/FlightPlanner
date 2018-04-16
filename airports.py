ba74d662b03aa5ce11b990c5d2d95394726a0059
Roger Lammers <rmlammers@gmail.com>
2018-04-16 19:05:26 +1200

    Fixed bug with distances being wrong. Now traverses the list to go to the closest airport next.

--- airports.py
+++ airports.py
@@ -1,27 +1,25 @@
 from geopy import distance
 
+ORIGIN = ('NZCH', -43.489444, 172.532222)
 AIRPORTS = [
     ('NZAA', -37.008056, 174.791667),
-    ('NZCH', -43.489444, 172.532222),
-    ('NZWN', -41.327222, 174.805278)
-    ]
-            # 'NZWN',
-            # 'NZQN',
-            # 'NZDN',
-            # 'NZNV',
-            # 'NZHN',
-            # 'NZRO',
-            # 'NZOH',
-            # 'NZWP',
-            # 'NZPM',
-            # 'NZNR',
-            # 'NZTG'
-            # ]
+    ('NZWN', -41.327222, 174.805278),
+    ('NZQN', -45.021111, 168.739167),
+    ('NZDN', -45.928056, 170.198333),
+    ('NZNV', -46.412222, 168.312778),
+    ('NZHN', -37.866667, 175.331944),
+    ('NZRO', -38.109167, 176.317222),
+    ('NZOH', -40.206111, 175.387778),
+    ('NZWP', -36.787778, 174.630278),
+    ('NZPM', -40.320556, 175.616944),
+    ('NZNR', -39.468333, 176.871667),
+    ('NZTG', -37.673333, 176.197222),
+]
 
 
 def distance_between(dept_airport, arr_airport):
     dept_coords = (dept_airport.latitude, dept_airport.longitude)
-    arr_coords = (arr_airport.latitude, dept_airport.longitude)
+    arr_coords = (arr_airport.latitude, arr_airport.longitude)
     return distance.great_circle(dept_coords, arr_coords).km
 
 
@@ -54,13 +52,18 @@ class Airport(object):
         self.longitude = longitude
 
 
-nz_airports = []
+dest_airports = []
+origin = Airport(ORIGIN[0], ORIGIN[1], ORIGIN[2])
+
 for airport in AIRPORTS:
     new_airport = Airport(airport[0], airport[1], airport[2])
-    nz_airports.append(new_airport)
+    dest_airports.append(new_airport)
 
 
-while len(nz_airports) > 1:
-    close_airport, close_dist = shortest_distance(nz_airports[0], nz_airports[1:])
-    print('Dept: ' + nz_airports[0].icao + " Dest: " + close_airport.icao + " " + str(close_dist))
-    nz_airports.remove(close_airport)
+previous_airport = origin
+
+while len(dest_airports) >= 1:
+    close_airport, close_dist = shortest_distance(previous_airport, dest_airports)
+    print('Dept: ' + previous_airport.icao + " Dest: " + close_airport.icao + " " + str(close_dist))
+    previous_airport = close_airport
+    dest_airports.remove(close_airport)
