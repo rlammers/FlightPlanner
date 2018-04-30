7d12498234983cc7421ebc6016f78e3c01c78c9f
Roger Lammers <rmlammers@gmail.com>
2018-04-30 21:42:03 +1200

    Added first test

--- test_airports.py
+++ test_airports.py
@@ -1,2 +1,12 @@
-from airports import Airport
+from airports import Airport, distance_between
 
+
+def test_distance_between():
+    christchurch = Airport('NZCH', -43.489444, 172.532222)
+    wellington = Airport('NZWN', -41.327222, 174.805278)
+    dist = distance_between(christchurch, wellington)
+    assert(int(dist) == 304)
+    dist = distance_between(wellington, christchurch)
+    assert(int(dist) == 304)
+    dist = distance_between(christchurch, christchurch)
+    assert(int(dist) == 0)
