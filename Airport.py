059827b38bf02ba5865366b558c4ed4b2c49e058
Roger Lammers <rmlammers@gmail.com>
2018-06-25 22:38:54 +1200

    Adding code for METAR string support

--- Airport.py
+++ Airport.py
@@ -3,9 +3,30 @@ class Airport(object):
     latitude = 0
     longitude = 0
     city = ''
+    metar = ''
 
     def __init__(self, icao, latitude, longitude, city):
         self.icao = icao
         self.latitude = latitude
         self.longitude = longitude
         self.city = city
+
+    def set_metar(self, metar):
+        self.metar = metar
+
+    def get_metar(self):
+        return  self.metar
+
+    @staticmethod
+    def validate_metar(metar):
+        metars = metar.split(' ')
+        if metars[0] == "METAR":
+            return True
+        else:
+            return False
+
+    def validate_icao(self, metar_icao):
+        if metar_icao == self.icao:
+            return True
+        else:
+            return False
