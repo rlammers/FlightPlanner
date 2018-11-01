8b13f05272ee17412f9fc8e3dfa8bb3f826bbb89
Rosjier Lammers <rmlammers@gmail.com>
2018-11-01 22:30:29 +1300

    Goal is to remove as many methods as possible from airports.py file, need to use distance_to instead of distance_between

--- Airport.py
+++ Airport.py
@@ -1,4 +1,6 @@
 from geojson import Point, Feature, FeatureCollection
+from geopy import distance
+from functools import lru_cache
 
 class Airport(object):
     icao = ''
@@ -18,3 +20,15 @@ class Airport(object):
         feature = Feature(id=None, geometry=point)
         feature_collection = FeatureCollection(features=[feature])
         return feature_collection
+
+    @lru_cache(maxsize=None)
+    def distance_to(self, destination, units):
+        departure_coords = (self.latitude, self.longitude)
+        arrival_coords = (destination.latitude, destination.longitude)
+        
+        if units.lower() == 'nm':
+            return distance.great_circle(departure_coords, arrival_coords).nm
+        elif units.lower() == 'km':
+            return distance.great_circle(departure_coords, arrival_coords).km
+        else:
+            raise ValueError('Invalid unit specified when calculating distance between airports.')
--- test_Airport.py
+++ test_Airport.py
@@ -9,3 +9,18 @@ def test_to_geojson():
     feature = Feature(id=None, geometry=point)
     feature_collection = FeatureCollection(features=[feature])
     assert(nzch.to_geojson() == feature_collection)
+
+def test_distance_to():
+    units = 'nm'
+    christchurch = Airport('NZCH', -43.489444, 172.532222, 'Christchurch')
+    wellington = Airport('NZWN', -41.327222, 174.805278, 'Wellington')
+    auckland = Airport('NZAA', -37.008056, 174.791667, 'Auckland')
+    invercargill = Airport('NZNV', -46.412222, 168.312778, 'Invercargill')
+    dist = christchurch.distance_to(wellington, units)
+    assert(int(dist) == 164)
+    dist = wellington.distance_to(christchurch, units)
+    assert(int(dist) == 164)
+    dist = christchurch.distance_to(christchurch, units)
+    assert(int(dist) == 0)
+    dist = auckland.distance_to(invercargill, units)
+    assert(int(dist) == 634)
