874ad330ec8fd3a3187257d1ae9e81283c72448c
Rosjier Lammers <rmlammers@gmail.com>
2018-11-20 22:38:03 +1300

    Added GeoJSON line string method to Flight, still needs testing.

--- flight.py
+++ flight.py
@@ -1,4 +1,14 @@
-class Flight(object):
+"""
+A single flight
+"""
+from geojson import LineString
+
+class Flight():
+    """
+    Flight object has an origin, destination and a calculated distance
+    between them. As well as the units for the distance. It can be printed
+    to a string or as GeoJSON.
+    """
     origin = None
     destination = None
     distance = None
@@ -13,3 +23,12 @@ class Flight(object):
     def __str__(self):
         return "-" + self.origin.city + " to " + self.destination.city + \
                " = " + str(int(self.distance)) + " " + self.units
+
+    def to_geojson(self):
+        """
+        Use GeoJSON library to create and return a line string representing the flight
+        """
+        origin_coordinates = (self.origin.longitude, self.origin.latitude)
+        destination_coordinates = (self.destination.longitude, self.destination.latitude)
+        line_string = LineString(coordinates=([origin_coordinates, destination_coordinates]))
+        return line_string
