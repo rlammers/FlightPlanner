f02c5842bd8ba7948edb7f28b85018342cdf907f
Roger Lammers <rmlammers@gmail.com>
2026-06-27 20:02:18 +1200

    Fixing some linting issues first.

--- airport.py
+++ airport.py
@@ -1,3 +1,7 @@
+"""
+Module to handle airport data including initialization, conversion to GeoJSON,
+distance calculation, and finding the closest airport.
+"""
 from functools import lru_cache
 
 from geojson import Point, Feature
@@ -5,27 +9,53 @@ from geopy import distance
 
 
 class Airport(object):
+    """
+    Represents an airport with attributes such as ICAO code, latitude,
+    longitude, and city. Provides methods for converting to GeoJSON format,
+    calculating distances to other airports, and finding the closest airport.
+    """
     icao = ''
     latitude = 0
     longitude = 0
     city = ''
 
     def __init__(self, icao, latitude, longitude, city):
+        """
+        Initialize an Airport object.
+
+        :param icao: The ICAO code of the airport.
+        :param latitude: The latitude of the airport.
+        :param longitude: The longitude of the airport.
+        :param city: The city where the airport is located.
+        """
         self.icao = icao
         self.latitude = latitude
         self.longitude = longitude
         self.city = city
 
     def to_geojson(self):
+        """
+        Convert the airport data to GeoJSON format.
+
+        :return: A GeoJSON Feature object representing the airport.
+        """
         point = Point([self.longitude, self.latitude])
         feature = Feature(id=None, geometry=point, properties={"city": self.city})
         return feature
 
     @lru_cache(maxsize=1024)
     def distance_to(self, destination, units):
+        """
+        Calculate the great-circle distance to another airport.
+
+        :param destination: The destination Airport object.
+        :param units: The unit of measurement for the distance ('nm' or 'km').
+        :return: The distance in the specified units.
+        :raises ValueError: If an invalid unit is provided.
+        """
         departure_coords = (self.latitude, self.longitude)
         arrival_coords = (destination.latitude, destination.longitude)
-        
+
         if units.lower() == 'nm':
             return distance.great_circle(departure_coords, arrival_coords).nm
         elif units.lower() == 'km':
@@ -34,6 +64,13 @@ class Airport(object):
             raise ValueError('Invalid unit specified when calculating distance between airports.')
 
     def closest_airport(self, airports, units):
+        """
+        Find the closest airport from a list of airports.
+
+        :param airports: A list of Airport objects.
+        :param units: The unit of measurement for the distance ('nm' or 'km').
+        :return: The closest Airport object.
+        """
         seen_dist = False
         closest_airport = None
         min_dist = 0
